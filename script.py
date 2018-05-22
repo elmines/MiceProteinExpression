61bb43c584f001831509702392def5e8d6218c31
Ethan Mines <elmines325@gmail.com>
2018-05-22 09:49:01 -0600

    WIP: Convert panda dataframe to TF dataset

--- script.py
+++ script.py
@@ -11,26 +11,45 @@ to_exclude = {"MouseID", "Treatment", "Behavior", "Genotype"}
 
 data_path = os.path.join("Data_Cortex_Nuclear.csv")
 
+
 with open(data_path, "r") as r:
     df = pd.read_csv(data_path, dtype=str)
 
 new_columns = set(df.columns) - to_exclude
 
 df = df[list(new_columns)]
-df = df.iloc[np.random.permutation(len(df))]
+df = df.iloc[np.random.permutation(len(df))] #Shuffle samples
 
-def parse_csv(line):
-    return line
 
-total_records = tf.constant(len(df))
+total_records = len(df)
 train_records = total_records * 2 // 3
 test_records = total_records - train_records
 
+train_df = df.iloc[ :train_records ]
+test_df = df.iloc[ train_records: ]
+
+label_dict = {label : i for (i, label) in enumerate( set(train_df["class"]) ) }
+
+def tf_dataset(dataframe, label_column, label_dict):
+    labels = np.asarray([ label_dict[class_name] for class_name in dataframe[label_column] ], dtype=np.float32 )
+
+    print("NEW:")
+    dataframe = dataframe[ list(set(dataframe.columns) - {label_column}) ]
+    print(dataframe)
+    print(labels)
+
+    for column in dataframe.columns: 
+
+
+tf_dataset(train_df.iloc[:10], "class", label_dict)
+
+
+#train_dataset = tf_dataset(train_df)
+"""
 with tf.Session() as sess:
     print(train_records.eval())
     print(test_records.eval())
 
-"""
 full_dataset = tf.data.TextLineDataset(data_path).skip(1).map(parse_csv).shuffle(2000)
 print(full_dataset)
 
