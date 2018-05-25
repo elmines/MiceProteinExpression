59a1ecdaf9725e7de43bf88e3f253b202d1bfb71
Ethan Mines <elmines325@gmail.com>
2018-05-25 15:42:56 -0400

    Set up logging of training loss

--- script.py
+++ script.py
@@ -36,9 +36,6 @@ def convert_nan(iterable):
     return np.asarray( [num_type(0) if "nan" in str(element).lower() else num_type(element) for element in iterable] )
 
 def tf_dataset(dataframe, label_column, label_dict):
-
-    #labels = np.asarray([ label_dict[class_name] for class_name in dataframe[label_column] ], dtype=num_type)
-
     labels = np.zeros( (len(dataframe), len(label_dict)), dtype=num_type)
     for i in range(len(dataframe)):
         index = label_dict[ dataframe[label_column].iloc[i] ]
@@ -51,18 +48,16 @@ def tf_dataset(dataframe, label_column, label_dict):
     for i in range(len(dataframe.columns)):
         features[ : , i] = convert_nan( dataframe[dataframe.columns[i]] )
 
-    #print(features.shape)
-
     dataset = tf.data.Dataset.from_tensor_slices( (features, labels) )
     return dataset
 
 
 
 label_dict = {label : i for (i, label) in enumerate( set(train_df["class"]) ) }
-short_df = train_df.iloc[:96]
 
+print(len(train_df))
 train_batch_size = 32
-train_dataset = tf_dataset(short_df, "class", label_dict).batch(train_batch_size)
+train_dataset = tf_dataset(train_df, "class", label_dict).batch(train_batch_size)
 
 input_dim = int(train_dataset.output_shapes[0][1])
 output_dim = len(label_dict)
@@ -70,59 +65,45 @@ def keras_model():
     return tf.keras.Sequential([
       tf.keras.layers.Dense(30, activation="relu", input_shape=(input_dim,)), 
       tf.keras.layers.Dense(30, activation="relu"),
-      #tf.keras.layers.Dense(output_dim)
       tf.keras.layers.Dense(output_dim, activation="softmax")
     ])
 
-def one_hot_vector(num_clases, indices):
-    vector = np.zeros(size, dtype=num_type)
-    vector[index] = 1
 
-def cross_entropy_loss(y_hat, y, axis=1):
-    loss = -tf.reduce_sum( tf.log(y_hat) * y, axis=axis )
+def cross_entropy_loss(y_hat, y):
+    loss = -tf.reduce_sum( tf.log(y_hat) * y, axis=1 )
     return loss
 
 def loss(model, x, y):
     return cross_entropy_loss(model(x), y)
 
-
-"""
-y_hat = np.asarray( [ [0.3, 0.3, 0.4], [0.7, 0.15, 0.15] ], dtype=num_type)
-y     = np.asarray( [ [0,   0,   1  ], [1,   0,    0   ] ], dtype=num_type)
-loss = cross_entropy_loss(model, y_hat, y)
-"""
-
-def running_average(curr_avg, num_samples, new_sample):
-    return ( curr_avg * num_samples + new_sample) / (num_samples + 1)
+def classification_accuracy(model, x, y):
+     return tf.argmax( model(x), axis=1) == tf.argmax( y, axis = 1)
 
 
 model = keras_model()
+
 iterator = train_dataset.make_one_shot_iterator()
 next_element = iterator.get_next()
 loss_value = loss(model, next_element[0], next_element[1])
+
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
 train_op = optimizer.minimize(loss_value)
+batch_loss = tf.reduce_mean(loss_value)
+
+test_batch_size = 1
+test_dataset = tf_dataset(test_df, "class", label_dict).batch(test_batch_size)
 
 with tf.Session() as sess:
     sess.run( tf.global_variables_initializer() )
-    #uninitialized = sess.run( tf.report_uninitialized_variables() )
-    #print(uninitialized)
-
+    i = 0
     try:
        while True:
-           sess.run(train_op)
-           """
-           features, label = pair
-           print(features)
-           print(label)
-           model_output = model()
-           print(model_output)
-           #print(model_output.eval())
-           break
-           gradients = loss_and_grad(model, features, label)
-           optimizer.apply_gradients( zip(gradients, model.variables), global_step = tf.train.get_or_create_global_step() )
-           """
-    except tf.errors.OutOfRangeError:
+           [batch_loss_output, _] = sess.run( [batch_loss, train_op] )
+           print("Batch {0}: Average loss = {1}".format(i, batch_loss_output), flush=True)
+           i += 1
+    except tf.errors.OutOfRangeError as e:
         pass
     
         
+
+
