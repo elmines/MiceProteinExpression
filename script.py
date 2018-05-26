555005f187f5d7d5bc05dbcd0ef960b7feb8831d
Ethan Mines <elmines325@gmail.com>
2018-05-26 15:25:03 -0400

    Customize batch size and number of epochs

--- script.py
+++ script.py
@@ -1,39 +1,58 @@
 import tensorflow as tf
+print("Tensorflow version: {0}".format(tf.VERSION))
 import os
 import sys
-
+from pydoc import locate #For determining our TensorFlow numeric types from our Numpy ones
 import numpy as np
 import pandas as pd
 
-num_type = np.float32
+#Seed random number generators
 seed = 1
 np.random.seed(seed)
 tf.set_random_seed(seed)
-print("Tensorflow version: {0}".format(tf.VERSION))
+
+#Set numeric types for operations
+num_type = np.float64
+tf_num_type = tf.as_dtype(num_type)
+
+#Minibatch Descent Hyperparameters
+minibatch_descent = False
+train_batch_size = 64 #Only used if minibatch_descent is True
+
+batch_ratio = 0.5 #Let each batch compose half the dataset
+
+valid_batch_size = 32
+num_epochs = 100
 
 
 # Read CSV file
 data_path = os.path.join("Data_Cortex_Nuclear.csv")
 with open(data_path, "r") as r:
     df = pd.read_csv(data_path, dtype=str)
-
-
 # Remove irrelevant or similar columns
 to_exclude = {"MouseID", "Treatment", "Behavior", "Genotype"}
 new_columns = set(df.columns) - to_exclude
 df = df[list(new_columns)]
+
 df = df.iloc[np.random.permutation(len(df))] #Shuffle samples
 
+#Partition dataset - 80% training, 10% validation, %10 testing
+num_samples = len(df)
+num_training = num_samples * 8 // 10
+if not minibatch_descent: # Scale batch sizes according to dataset size
+    train_batch_size = int(num_training * batch_ratio) if batch_ratio else num_training
 
-#Partition dataset
-total_records = len(df)
-train_records = total_records * 2 // 3
-train_df = df.iloc[ :train_records ]
-test_df = df.iloc[ train_records: ]
+num_validation = num_samples // 10
+num_testing = num_samples - num_training - num_validation
+train_df = df.iloc[ :num_training ]
+validation_df = df.iloc[ num_training : num_training + num_validation ]
+test_df = df.iloc[ num_training + num_validation : ]
+print("{0} samples: {1} training, {2} validation, {3} testing".format(num_samples, num_training, num_validation, num_testing), flush=True)
+
 
 
 def convert_nan(iterable):
-    return np.asarray( [num_type(0) if "nan" in str(element).lower() else num_type(element) for element in iterable] )
+    return np.asarray( [num_type(0) if "nan" in str(element).lower() else num_type(element) for element in iterable], dtype=num_type )
 
 def tf_dataset(dataframe, label_column, label_dict):
     labels = np.zeros( (len(dataframe), len(label_dict)), dtype=num_type)
@@ -41,69 +60,58 @@ def tf_dataset(dataframe, label_column, label_dict):
         index = label_dict[ dataframe[label_column].iloc[i] ]
         labels[i][index] = 1
 
-
     dataframe = dataframe[ list(set(dataframe.columns) - {label_column}) ]
-
     features = np.ndarray( (len(dataframe), len(dataframe.columns)) , dtype=num_type )
     for i in range(len(dataframe.columns)):
         features[ : , i] = convert_nan( dataframe[dataframe.columns[i]] )
 
-    dataset = tf.data.Dataset.from_tensor_slices( (features, labels) )
-    return dataset
+    return tf.data.Dataset.from_tensor_slices( (features, labels) )
 
 
 
 label_dict = {label : i for (i, label) in enumerate( set(train_df["class"]) ) }
-
-print(len(train_df))
-train_batch_size = 32
 train_dataset = tf_dataset(train_df, "class", label_dict).batch(train_batch_size)
+test_dataset = tf_dataset(test_df, "class", label_dict).batch(1)
 
 input_dim = int(train_dataset.output_shapes[0][1])
 output_dim = len(label_dict)
 def keras_model():
     return tf.keras.Sequential([
-      tf.keras.layers.Dense(30, activation="relu", input_shape=(input_dim,)), 
-      tf.keras.layers.Dense(30, activation="relu"),
-      tf.keras.layers.Dense(output_dim, activation="softmax")
+      tf.keras.layers.Dense(30, activation="relu", input_shape=(input_dim,), dtype=tf_num_type), 
+      tf.keras.layers.Dense(30, activation="relu", dtype=tf_num_type),
+      tf.keras.layers.Dense(output_dim, activation="softmax", dtype=tf_num_type)
     ])
 
 
 def cross_entropy_loss(y_hat, y):
-    loss = -tf.reduce_sum( tf.log(y_hat) * y, axis=1 )
-    return loss
-
+    return -tf.reduce_sum( tf.log(y_hat) * y, axis=1 )
 def loss(model, x, y):
     return cross_entropy_loss(model(x), y)
-
 def classification_accuracy(model, x, y):
      return tf.argmax( model(x), axis=1) == tf.argmax( y, axis = 1)
 
 
-model = keras_model()
-
-iterator = train_dataset.make_one_shot_iterator()
+iterator = tf.data.Iterator.from_structure(train_dataset.output_types, train_dataset.output_shapes)
+training_init_op = iterator.make_initializer(train_dataset)
 next_element = iterator.get_next()
-loss_value = loss(model, next_element[0], next_element[1])
 
+model = keras_model()
+loss_value = loss(model, next_element[0], next_element[1])
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
 train_op = optimizer.minimize(loss_value)
 batch_loss = tf.reduce_mean(loss_value)
 
-test_batch_size = 1
-test_dataset = tf_dataset(test_df, "class", label_dict).batch(test_batch_size)
 
+def num_batches(num_samples, batch_size):
+    return num_samples // batch_size + (0 if num_samples % batch_size == 0 else 1)
+
+train_batches = num_batches(num_training, train_batch_size)
+print("Training on %d samples divided into batches of %d" % (num_training, train_batches))
 with tf.Session() as sess:
     sess.run( tf.global_variables_initializer() )
-    i = 0
-    try:
-       while True:
-           [batch_loss_output, _] = sess.run( [batch_loss, train_op] )
-           print("Batch {0}: Average loss = {1}".format(i, batch_loss_output), flush=True)
-           i += 1
-    except tf.errors.OutOfRangeError as e:
-        pass
-    
-        
-
-
+    for epoch in range(num_epochs):
+        sess.run( training_init_op )
+        print("EPOCH {0}".format(epoch))
+        for i in range(train_batches):
+            [batch_loss_output, _] = sess.run( [batch_loss, train_op] )
+            print("\tBatch {0}: Average loss = {1}".format(i, batch_loss_output), flush=True)
